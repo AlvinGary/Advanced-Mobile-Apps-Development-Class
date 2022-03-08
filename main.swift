1ed4d5215b994089592a65d898824600d37ae445
AlvinGary <71222366+AlvinGary@users.noreply.github.com>
2022-03-08 01:48:04 +0000

    Operators and Control Flows

--- main.swift
+++ main.swift
@@ -1,16 +1,87 @@
-// print("Welcome to Mobile Legends!")
+// Bollean operator: And (&&), OR (||), NOT (!)
 
-// var isBanned: Bool = false
-// print(isBanned)
 
-// var winRate: Double = 45.36
-// print(winRate)
+// var temperature = 75
 
-// winRate = 52.76
-// print(winRate)
+// if (temperature >= 65) && (temperature <= 75) {
+//   print("The water is boiling")
+// } else if temperature < 65{
+//   print("It is too cold")
+// } else {
+//   print("It is too hot")
+// }
 
-// let heroName: String = "Tigreal"
-// print(heroName)
 
-let 👌: String = "naisu"
-print(👌)
+// var isRain = false
+
+// if !isRain {
+//   print("Bring your umbrella")
+// }
+
+
+// var numberOfWheels = 2
+
+// switch numberOfWheels {
+//   case 0:
+//     print("Missing wheels")
+//   case 1:
+//     print("Unicycle")
+//   case 2:
+//     print("Bicycle")
+//   default:
+//     print("That's a lot of wheels")
+// }
+
+
+// print(Int.max)
+// print(Int8.max)
+// print(Int16.max)
+// print(Int32.max)
+// print(Int64.max)
+
+
+// let distance: Int = 25
+
+// switch distance {
+//   case 0...9:
+//   print("Your destination is close")
+//   case 10...99:
+//   print("Your destination is not that far")
+//   case 100...999:
+//   print("Your destination is far from here")
+//   default:
+//   print("Where are you going?")
+// }
+
+
+// let letter: Character = "x"
+
+// switch letter {
+//   case "a", "i", "u", "e", "o":
+//   print("The letter is vowel")
+//   default:
+//   print("The letter is a consonant")
+// }
+
+
+// var largerNumber: Int
+
+// let number1 = 40
+// let number2 = 5
+
+// // if number1 > number2 {
+// //   largerNumber = number1
+// // } else {
+// //   largerNumber = number2
+// // }
+
+//   // Ini adalah bentuk dari ternary operator
+// largerNumber = number1 > number2 ? number1 : number2
+
+// // String literal
+// print("larger number is: \(largerNumber)")
+// print("larger number is:", (largerNumber))
+
+// Math Operators: *, +, -, %
+//Bool operators: &&, ||, !
+//Ternary operators: ?, :
