9a0bad829086838d6e7c32f80e5096e882c5a629
alvingary201 <>
2022-03-08 00:48:32 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,16 @@
+// print("Welcome to Mobile Legends!")
+
+// var isBanned: Bool = false
+// print(isBanned)
+
+// var winRate: Double = 45.36
+// print(winRate)
+
+// winRate = 52.76
+// print(winRate)
+
+// let heroName: String = "Tigreal"
+// print(heroName)
+
+let 👌: String = "naisu"
+print(👌)
